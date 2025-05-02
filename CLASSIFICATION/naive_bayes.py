0a8cce57d3600a6653366a8c15f3e3fdaafc8ac4
Antithetical <arthur.guelennoc@epita.fr>
2025-05-02 15:33:34 +0200

    feat: added naive bayes + renamed logistic regression

---
+++ CLASSIFICATION/naive_bayes.py
@@ -0,0 +1,36 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pandas as pd
+
+def load_data(path):
+    dataframe = pd.read_csv(path)
+    return dataframe
+
+def train_model(path):
+    data = load_data(path)
+    X = data["sentence"].values
+    y = data["explicitness"] - 1
+    vectorizer = TfidfVectorizer()
+    X_vectorized = vectorizer.fit_transform(X)
+    model = MultinomialNB()
+    model.fit(X_vectorized, y)
+    return model, vectorizer
+
+def predict(path, sentence):
+    model, vectorizer = train_model(path)
+    input_vector = vectorizer.transform([sentence])
+    return model.predict(input_vector)[0] + 1
+
+def main():
+    path = "../data/GOAT.csv"
+    sentences = {
+        "This book belongs on the top shelf. 😊": 1,
+        "I'm not sure if this book belongs on the top shelf... 🤔": 2,
+        "Right, because heavy textbooks at eye level is such a brilliant idea!": 3,
+    }
+    for sentence, explicitness in sentences.items():
+        prediction = predict(path, sentence)
+        print(f"Prediction: {prediction} | Expected: {explicitness} | Sentence: {sentence}")
+
+if __name__ == "__main__":
+    main()
